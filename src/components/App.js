9e1307fa8f0baee1c94246e2f4003b6df77c541f
joyce-shi <j224shi@uwaterloo.ca>
2020-05-25 22:58:06 -0400

    setting up order totalling

--- src/components/App.js
+++ src/components/App.js
@@ -25,16 +25,33 @@ class App extends React.Component {
     })
   }
 
+  addToOrder = (key) => {
+    const order = {...this.state.order};
+    order[key] = order[key] + 1 || 1;
+    this.setState({
+      order
+    });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
-            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+            {
+              Object.keys(this.state.fishes).map(key =>
+                <Fish
+                  addToOrder={this.addToOrder}
+                  details={this.state.fishes[key]}
+                  index={key}
+                  key={key} // recall: can't access key property from inside the component
+                />
+              )
+            }
           </ul>
         </div>
-          <Order />
+          <Order fishes={this.state.fishes} order={this.state.order}/>
           <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     );
